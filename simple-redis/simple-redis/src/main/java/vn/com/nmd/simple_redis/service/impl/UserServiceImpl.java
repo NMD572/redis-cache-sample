4473534eaf5b9192b01ffa03d56720b44e9d96d2
Nguyen Minh Dang <minhdangb3@gmail.com>
2024-12-18 15:14:41 +0700

    [DangNM] push metada file (not need)

--- simple-redis/simple-redis/src/main/java/vn/com/nmd/simple_redis/service/impl/UserServiceImpl.java
+++ simple-redis/simple-redis/src/main/java/vn/com/nmd/simple_redis/service/impl/UserServiceImpl.java
@@ -18,19 +18,23 @@ public class UserServiceImpl implements IUserService {
 	@Autowired
     private UserRepository userRepository;
 
+	// key can only use input value 
 	@Override
     @Cacheable(value = "users", key = "#id")		// get user from cache by key = id
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
 
+
+	// key can only use input value 
 	@Override
     @Transactional(rollbackFor = Exception.class)
-    @CachePut(value = "users", key = "#user.id")	// add information to cache when create / update information to cache when update
+    @CachePut(value = "users", key = "#userResult.id")	// add information to cache when create / update information to cache when update
     public User saveUser(User user) {
-        return userRepository.save(user);
+		return userRepository.save(user);
     }
 
+	// key can only use input value 
 	@Override
     @Transactional(rollbackFor = Exception.class)
     @CacheEvict(value = "users", key = "#id")		// remove from cache when delete user
